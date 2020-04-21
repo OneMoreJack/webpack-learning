90b358103d327d566b35fbe29bba7faf2f33b4d6
JackLiR8 <jiakeli1024@gmail.com>
2020-04-21 15:51:38 +0800

    :construction: 处理css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import * as R from 'ramda'
+import './style.css'
+
 function component() {
   const ele = document.createElement('div')
-  
+
   ele.innerText = R.join(',', ['Hello', 'Webpack!'])
+  ele.classList.add('hello')
 
   return ele
 }
